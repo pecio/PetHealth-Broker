d4064f29949042c1bf46f8c047d604f1d21faea1
Raúl Pedroche <r.pedroche.novillo@gmail.com>
2014-03-31 20:38:29 +0200

    Fidn pet info through pet journal api

--- core.lua
+++ core.lua
@@ -11,16 +11,40 @@ f:SetScript("OnUpdate", function(self, elap)
   if elapsed < UPDATEPERIOD then return end
 
   elapsed = 0
-  local c1 = C_PetBattles.GetHealth(1, 1)
-  local c2 = C_PetBattles.GetHealth(1, 2)
-  local c3 = C_PetBattles.GetHealth(1, 3)
-  local m1 = C_PetBattles.GetMaxHealth(1, 1)
-  local m2 = C_PetBattles.GetMaxHealth(1, 2)
-  local m3 = C_PetBattles.GetMaxHealth(1, 3)
-  local i1 = C_PetBattles.GetIcon(1, 1)
-  local i2 = C_PetBattles.GetIcon(1, 2)
-  local i3 = C_PetBattles.GetIcon(1, 3)
-  dataobj.text = string.format("|T%s:16|t %d/%d", i1, c1, m1)
+  --local c1 = C_PetBattles.GetHealth(1, 1)
+  --local c2 = C_PetBattles.GetHealth(1, 2)
+  --local c3 = C_PetBattles.GetHealth(1, 3)
+  --local m1 = C_PetBattles.GetMaxHealth(1, 1)
+  --local m2 = C_PetBattles.GetMaxHealth(1, 2)
+  --local m3 = C_PetBattles.GetMaxHealth(1, 3)
+  --local i1 = C_PetBattles.GetIcon(1, 1)
+  --local i2 = C_PetBattles.GetIcon(1, 2)
+  --local i3 = C_PetBattles.GetIcon(1, 3)
+  total, owned = C_PetJournal.GetNumPets
+  current = {}
+  max = {}
+  icon = {}
+  slot = 1
+  for i = 1, total do
+    petID, speciesID, owned, customName, level, favorite, isRevoked, speciesName, icon, petType, companionID, tooltip, description, isWild, canBattle, isTradeable, isUnique, obtainable = C_PetJournal.GetPetInfoByIndex(i)
+    if owned then
+      slotted = C_PetJournal.PetIsSlotted(petID)
+      if slotted then
+        health, maxHealth, power, speed, rarity = C_PetJournal.GetPetStats("petID")
+        current[slot] = health
+        max[slot] = maxHealth
+
+        speciesName, speciesIcon, petType, companionID, tooltipSource, tooltipDescription, isWild, canBattle, isTradeable, isUnique, obtainable = C_PetJournal.GetPetInfoBySpeciesID(speciesID)
+        icon[slot] = speciesIcon
+
+        slot = slot + 1
+        if slot > 3 then
+          break
+        end
+      end
+    end
+  end
+  dataobj.text = string.format("|T%s:16|t %d/%d", icon[1], current[1], max[1])
 end)
 
 function dataobj:OnTooltipShow()
