3eccbc080106b264338c5b13f77e4080c06c4b98
Raúl Pedroche <r.pedroche.novillo@gmail.com>
2014-04-02 01:24:55 +0200

    Update every half second (so cooldown looks better)

--- core.lua
+++ core.lua
@@ -4,7 +4,7 @@ local ldb = LibStub:GetLibrary("LibDataBroker-1.1")
 local AceConfig = LibStub("AceConfig-3.0")
 local AceConfigReg = LibStub("AceConfigRegistry-3.0")
 
-local UPDATEPERIOD, elapsed = 2, 0
+local UPDATEPERIOD, elapsed = 0.5, 0
 local dataobj = ldb:NewDataObject("Pet Health", { type = "data source", text = "Pet Health Info"})
 local f = CreateFrame("frame")
 
