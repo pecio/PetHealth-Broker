18deb2991e4abd807fd33d94e15e25a0c0e9caf3
Raúl Pedroche <r.pedroche.novillo@gmail.com>
2014-04-02 12:21:27 +0200

    esES localization for new notifications

--- Locales/esES.lua
+++ Locales/esES.lua
@@ -13,8 +13,16 @@ L['Show Rarity'] = 'Mostrar Rareza'
 L['Colorize max health or percent sign based on pet rarity'] = 'Colorear la salud máxima o el simbolo de tanto porciento basándonos en la rareza de la mascota'
 L['Show Cooldown'] = 'Mostrar "Cooldown"'
 L['Show cooldown time for Revive Battle Pets spell in bar'] = 'Mostrar el tiempo de reutilización restante de la habilidad de Revivir Mascotas'
-L['Play Sound Alert'] = 'Alerta de Sonido'
-L['Play the Level Up sound when cooldown finishes'] = 'Reproducir el sonido de Subir de Nivel cuando el tiempo de reutilización termina'
+-- Revive Battle Pets availability notification config
+L['Notify Availability'] = 'Notificar disponibilidad'
+L['Notify the player when cooldown time finishes'] = 'Notificar al jugador cuando el tiempo de reutilización de la habilidad acaba'
+L['None'] = 'Ninguna'
+L['With Level Up sound'] = 'Con sonido Subir de Nivel'
+L['In chat'] = 'En el chat'
+L['Both'] = 'Ambos'
+
+-- In chat notification of RBP ready; %s us RBP name
+L['%s is ready'] = '%s está listo'
 
 -- Click instructions
 L['Left Click to open Pet Journal'] = 'Click Izquierdo para abrir el Diario de Mascotas'
