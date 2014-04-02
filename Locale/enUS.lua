d1c8b4f47953ea29a4670023798d6faf7164e29c
Raúl Pedroche <r.pedroche.novillo@gmail.com>
2014-04-02 05:38:39 +0200

    Add enUS localization

---
+++ Locale/enUS.lua
@@ -0,0 +1,20 @@
+-- enUS (default) localization
+local L = LibStub("AceLocale-3.0"):NewLocale("PetHealthBroker", "enUS", true)
+
+-- Title for DataBroker
+L['Pet Health'] = true
+
+-- Configuration
+L['Show Percentages'] = true
+L['Show Percentages instead of current/max health'] = true
+L['Show Rarity'] = true
+L['Colorize max health or percent sign based on pet rarity'] = true
+L['Show Cooldown'] = true
+L['Show cooldown time for Revive Battle Pets spell in bar'] = true
+L['Play Sound Alert'] = true
+L['Play the Level Up sound when cooldown finishes'] = true
+
+-- Click instructions
+L['Left Click to open Pet Journal'] = true
+L['Right Click to open Options'] = true
+L['Control-Left Click to rearrange pets by health'] = true
