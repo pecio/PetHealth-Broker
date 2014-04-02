5b17a8f76626841acc4bdf25c0ccafd8de0f9ae9
Raúl Pedroche <r.pedroche.novillo@gmail.com>
2014-04-02 06:08:28 +0200

    Add esES localization

---
+++ Locales/esES.lua
@@ -0,0 +1,20 @@
+-- esES localization
+local L = LibStub("AceLocale-3.0"):NewLocale("PetHealthBroker", "esES")
+
+-- Title for DataBroker
+L['Pet Health'] = 'Salud de Mascota'
+
+-- Configuration
+L['Show Percentages'] = 'Mostrar Porcentajes'
+L['Show Percentages instead of current/max health'] = 'Mostrar Porcentages en lugar de salud actual/máxima'
+L['Show Rarity'] = 'Mostrar Rareza'
+L['Colorize max health or percent sign based on pet rarity'] = 'Colorear la salud máxima o el simbolo de tanto porciento basándonos en la rareza de la mascota'
+L['Show Cooldown'] = 'Mostrar "Cooldown"'
+L['Show cooldown time for Revive Battle Pets spell in bar'] = 'Mostrar el tiempo de reutilización restante de la habilidad de Revivir Mascotas'
+L['Play Sound Alert'] = 'Alerta de Sonido'
+L['Play the Level Up sound when cooldown finishes'] = 'Reproducir el sonido de Subir de Nivel cuando el tiempo de reutilización termina'
+
+-- Click instructions
+L['Left Click to open Pet Journal'] = 'Click Izquierdo para abrir el Diario de Mascotas'
+L['Right Click to open Options'] = 'Click Derecho para abrir Opciones'
+L['Control-Left Click to rearrange pets by health'] = 'Control-Click derecho para recolocar las mascotas en base a su salud'
